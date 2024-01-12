29f9c9c5c9ad57cc517ab8c59b6920123a4dfc18
K-kare <kamunawangari@gmail.com>
2024-01-12 14:11:34 -0800

    9-main.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    current_dir = a_dictionary.copy()
+    list_key = list(current_dir.keys())
+    for i in list_key:
+        current_dir[i] *= 2
+        return (current_dir)
