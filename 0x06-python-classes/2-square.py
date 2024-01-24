58ddff1ea76df8e8a84ef23679c64d14b680d849
K-kare <kamunawangari@gmail.com>
2024-01-24 06:30:54 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,6 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square
         """
@@ -17,4 +16,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
